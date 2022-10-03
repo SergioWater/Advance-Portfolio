3996174eee758f7bfbc7f49ec86264278e56a4e2
Sergio <sergiof_2001@hotmail.com>
2022-10-03 15:41:39 -0700

    made the loading screen work

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 // template_2cvjmfg
 // service_a2vjxdm
 // wA9t9jTv7Oqs1AFxa
+function contact(event){
+    event.preventDefault()
+    // emailjs
+    //     .sendForm(
+    //         'service_a2vjxdm',
+    //         'template_2cvjmfg',
+    //          event.target,
+    //          'wA9t9jTv7Oqs1AFxa'
+    //     ).then(() => {
+    //         console.log('this worked')
+    //     })
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += ' modal__overlay--visible'
+    setTimeout(() =>{
+        console.log('it worked')
+    }, 500)
+}
