8cc7c11c7367597880826c7d18536d9165d3c90f
Sergio <sergiof_2001@hotmail.com>
2022-10-03 23:13:39 -0700

    added the toggleModal() funnction

--- index.js
+++ index.js
@@ -3,19 +3,33 @@
 // wA9t9jTv7Oqs1AFxa
 function contact(event){
     event.preventDefault()
-    // emailjs
-    //     .sendForm(
-    //         'service_a2vjxdm',
-    //         'template_2cvjmfg',
-    //          event.target,
-    //          'wA9t9jTv7Oqs1AFxa'
-    //     ).then(() => {
-    //         console.log('this worked')
-    //     })
     const loading = document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
     loading.classList += ' modal__overlay--visible'
-    setTimeout(() =>{
-        console.log('it worked')
-    }, 500)
+    emailjs
+        .sendForm(
+            'service_a2vjxdm',
+            'template_2cvjmfg',
+             event.target,
+             'wA9t9jTv7Oqs1AFxa'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible"
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailble. Please contact me directly on sergiowbjr@gmail.com"
+            )
+        })
+}
+let isModalOpen = false;
+function toggleModal(){
+
+    if(isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove('modal--open')
+    }
+    isModalOpen = true;
+
+    document.body.classList += " modal--open"
 }
